c8c2c5c97ad4b08a0f8557ee62ee847d5983feff
BlueSky1234 <yahshuaharris@icloud.com>
2025-04-22 09:01:15 -0700

    Finish Project

--- index.js
+++ index.js
@@ -20,8 +20,30 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+app.get("/api", (req, res) => {
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  })
+})
+
+
+app.get("/api/:date?", (req, res) => {
+  let dateParam = req.params.date
+  
+  if (!isNaN(dateParam)) {
+    dateParam = parseInt(dateParam);
+  }
+
+  const date = new Date(dateParam);
+
+  if (date.toString() === "Invalid Date") {
+    res.json({error: "Invalid Date"})
+  } else {
+    res.json({unix: date.getTime(), utc: date.toUTCString()})
+  }
 });
 
 
